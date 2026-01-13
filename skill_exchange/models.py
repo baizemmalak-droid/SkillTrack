930d7a4a4a2a25e32a0d2636f7d64c4e9f6f1070
Salma Bankodad <Bankodad.salma@etu.uae.ac.ma>
2026-01-13 21:45:42 +0100

    Skill request form working with redirect and basic UI

--- skill_exchange/models.py
+++ skill_exchange/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
 
 class Skill(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
 
+
 class SkillRequest(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)  # الوقت الذي تم فيه الطلب
+    skill_name = models.CharField(max_length=100)
+    description = models.TextField()
 
     def __str__(self):
-        return f"{self.user.username} requests {self.skill.name}"
+        return self.skill_name
