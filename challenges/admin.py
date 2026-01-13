a1599ee5291d58c2fee55f4039284929daf559aa
afylal-manar <afylal.manar@etu.uae.ac.ma>
2026-01-14 00:35:31 +0100

    Add challenges, dashboard, leaderboard and auth logic

--- challenges/admin.py
+++ challenges/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Challenge, UserChallenge
 
-admin.site.register(Challenge)
-admin.site.register(UserChallenge)
+@admin.register(Challenge)
+class ChallengeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'points')
+    search_fields = ('title',)
+    list_filter = ('points',)
+
+@admin.register(UserChallenge)
+class UserChallengeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'challenge', 'completed', 'completed_at')
+    list_filter = ('completed',)
 
