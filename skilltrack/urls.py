b7586a0d5e1fac2e774bf04ddffa95ce65930423
baizem malak <baizem.malak@etu.uae.ac.ma>
2026-01-14 16:14:44 +0100

    Add user profile and dashboard with authentication protection

--- skilltrack/urls.py
+++ skilltrack/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('students.urls')),
+
+    path('', include('accounts.urls')),   # ⬅️ الأول
+    path('', include('students.urls')),   # ⬅️ الثاني
 ]
