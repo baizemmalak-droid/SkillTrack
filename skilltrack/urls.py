8111209dff68fe1b791e439d1b00adeff29c4258
bankodadsalma-commits <bankodad.salma@etu.uae.ac.ma>
2026-01-17 17:49:21 +0100

    Delete skilltrack directory

--- skilltrack/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('students.urls')),   # profile / dashboard
-    path('', include('accounts.urls')),   # login / register / home
-]
