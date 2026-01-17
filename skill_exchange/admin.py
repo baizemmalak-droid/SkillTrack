9dc3c112a0fcf000f69cb53339d538d915b001b0
bankodadsalma-commits <bankodad.salma@etu.uae.ac.ma>
2026-01-17 17:44:13 +0100

    Delete skill_exchange directory

--- skill_exchange/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Skill, SkillRequest
-
-admin.site.register(Skill)
-admin.site.register(SkillRequest)
-
--- skill_exchange/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.skills_home, name='skills_home'),
-    path('request/', views.request_skill, name='request_skill'),
-]
