b8454535fbfbd658ea8cca098c81900b09ca717c
afylal-manar <afylal.manar@etu.uae.ac.ma>
2026-01-17 20:37:51 +0100

    clean project

--- students/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('dashboard/', views.dashboard_view, name='dashboard'),
-    path('profile/', views.profile_view, name='profile'),
-    path('profile/edit/', views.edit_profile, name='edit_profile'),
-]
